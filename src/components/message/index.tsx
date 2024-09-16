a063208d8cbb7ac1696c77622a46d0d40750e0bb
kielllll <eatarranza@gmail.com>
2024-09-16 22:40:30 +0800

    feat: use a markdown parser

--- src/components/message/index.tsx
+++ src/components/message/index.tsx
@@ -1,5 +1,8 @@
 import { cn, formatISO } from '@/lib/utils'
 import { memo } from 'react'
+import Markdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 interface MessageProps {
   id: string
@@ -23,9 +26,34 @@ function Message(props: MessageProps) {
         isUser ? 'bg-primary' : 'bg-secondary'
       )}
     >
-      <p className={cn('text-sm', isUser ? 'text-secondary' : 'text-primary')}>
+      {/* <p className={cn('text-sm', isUser ? 'text-secondary' : 'text-primary')}>
         {props.content}
-      </p>
+      </p> */}
+      <Markdown
+        className={cn('text-sm', isUser ? 'text-secondary' : 'text-primary')}
+        components={{
+          code({ className, children, ref, style, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return match ? (
+              <SyntaxHighlighter
+                language={match[1]}
+                customStyle={{ padding: '10px', borderRadius: '5px' }}
+                PreTag="div"
+                {...props}
+                style={dark}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          },
+        }}
+      >
+        {props.content}
+      </Markdown>
       <span className="text-xs text-muted-foreground">
         {formatISO(props.createdAt)}
       </span>
