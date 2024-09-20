4fb7378eaa89a7769f0625b7000a0c27072b8af9
kielllll <eatarranza@gmail.com>
2024-09-21 00:09:30 +0800

    fix: laptop screen layout

--- src/app/_components/sidebar.tsx
+++ src/app/_components/sidebar.tsx
@@ -30,7 +30,7 @@ export default function SideBar() {
   }
 
   return (
-    <aside className="lg:w-3/12 xl:w-2/12 md:3/5 border-r-[1px] hidden sm:block max-h-screen overflow-y-auto">
+    <aside className="lg:w-3/12 md:3/5 border-r-[1px] hidden sm:block max-h-screen overflow-y-auto">
       <div className="w-full bg-gray-800 p-4">
         <div className="flex w-full justify-end items-center gap-1">
           <h1 className="text-2xl font-bold mr-auto">ChadGPT</h1>
